1f58ba8a046aa07d552d94d1f4895c4fd9d5326e
Magnus <magnus.erler@gmail.com>
2022-05-24 17:49:49 +0200

    Able to remove elements by ID and Class-name

--- popup.js
+++ popup.js
@@ -18,6 +18,14 @@ changeColor.addEventListener("click", async () => {
     function: setPageBackgroundColor,
   });
 });
+
+// The body of this function will be executed as a content script inside the current page
+function setPageBackgroundColor() {
+
+  chrome.storage.sync.get("color", ({ color }) => {
+    document.body.style.backgroundColor = color;
+  });
+}
   
 function getElementByXpath(path) {
   return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
@@ -28,23 +36,35 @@ function getElementByXpath(path) {
 removeStore.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-  
-  
-  element = getElementByXpath('//*[@id="root"]/div[1]/div/div[2]/div[2]/div/div[2]/a[2]/div[2]/span');
-
-  //<span class="_7CHGi"><a role="store" class="ox0KZ" href="//www.aliexpress.com/store/911604466" target="_blank">Shop911604466 Store</a></span>
-  <span class="_7CHGi"><a role="store" class="ox0KZ" href="//www.aliexpress.com/store/911604466" target="_blank">Shop911604466 Store</a></span>
-
-  document.getElementById("debugMsg").innerHTML = element;
-
-  element.remove();
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: removeElement,
+  });
 });
 
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
+// The body of this function will be executed as a content script inside the current page
+function removeElement() {
 
-  chrome.storage.sync.get("color", ({ color }) => {
+  //element = getElementByXpath('//*[@id="root"]/div[1]/div/div[2]/div[2]/div/div[2]/a[2]/div[2]/span');
+  //element = getElementByXpath('//*[@id="home-firstscreen"]');
+
+  //<span class="_7CHGi"><a role="store" class="ox0KZ" href="//www.aliexpress.com/store/911604466" target="_blank">Shop911604466 Store</a></span>
+
+  //document.getElementById("debugMsg").innerHTML = element;
+
+  
+  //Remove ID
+  //var element = document.getElementById('root');
+  //element.remove();
+
+  //Remove Class
+  const elements = document.getElementsByClassName("_7CHGi");
+  while(elements.length > 0){
+      elements[0].parentNode.removeChild(elements[0]);
+  }
+  
+
+  /*chrome.storage.sync.get("color", ({ color }) => {
     document.body.style.backgroundColor = color;
-  });
+  });*/
 }
